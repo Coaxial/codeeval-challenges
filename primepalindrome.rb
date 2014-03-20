9fc9d911a2624d518035e9acc972a609b6db7e5a
Pierre-Yves Poujade <py@poujade.org>
2014-03-20 10:42:27 -0400

    prints palindrom primes to 1000

--- primepalindrome.rb
+++ primepalindrome.rb
@@ -1,6 +1,5 @@
-# finding all prime numbers to 1000
+# finding all palindrome prime numbers to 1000
 require 'prime'
 Prime.each(1000) do |prime|
-  print prime
-  print ' '
+  print "#{prime} " if prime.to_s == prime.to_s.reverse
 end
